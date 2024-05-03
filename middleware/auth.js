1b30e66fadcc4f43ce839717b67b596f7dd73029
Kage <dumitruwebdev@gmail.com>
2024-05-03 23:56:39 +0300

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,6 @@
 module.exports = function(req, res, next) {
     if(!req.session.isAuth){
-        req.session.isAuthenticated = false;
-        // return res.redirect('/auth/login');
-    } else {
-        req.session.isAuthenticated = true;
+        return res.redirect('/auth/login');
     }
     
     next();
